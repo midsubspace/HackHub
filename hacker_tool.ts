74aefdf211a815e65b56244923ff5a03a67fa05d
Midsubspace <midsubspace@gmail.com>
2026-08-04 18:35:24 -0500

    shit down

--- hacker_tool.ts
+++ hacker_tool.ts
@@ -1,12 +1,17 @@
-const args =Shell.GetArgs();
+let args =Shell.GetArgs();
 let target: string|null = null;
-if (args[0]==`-url`){
-    let url = args[1];
-    await Shell.Process.exec(`nslookup ${url}`);
-    target = await prompt("IP Address: ")
-}else {
-    target=args[1]
+if (args.length == 0){
+    args = (await prompt(`URL/IP ADDRESS: `))
 }
-
-await Shell.Process.exec(`nmap ${target}`)
-await Shell.Process.exec(`probe ${target}`)
+if (!Networking.IsIp(args[0])){
+    let url = args[0];
+    await Shell.Process.exec(`nslookup ${url}`);
+    await Shell.Process.exec(`nslookup ${url} > temp.txt`);
+    const lines = await FileSystem.ReadFile(`temp.txt`);
+    target = (`${lines}`.split(`Address:	`)[1]);
+}else {
+    target=args[0];
+}
+await Shell.Process.exec(`whois ${target}`);
+await Shell.Process.exec(`nmap ${target} -sV`);
+await Shell.Process.exec(`probe ${target}`);
--- wifi.ts
+++ wifi.ts
@@ -2,18 +2,21 @@ const mon: string = `wlan0`
 const isInstalled = await installLib("hashcat");
 println(`Hash Cat: ${isInstalled}`);
 println(`Network Interface: ${mon}`)
-let target: object|null = null;
+let target: object|string = `null`;
 const networks = await Networking.Wifi.Scan(mon);
 for ( let network of networks){
-   if (network.signal>2){
+   if (network.signal>1){
     target = network
+    await Networking.Wifi.Deauth(mon,target.bssid,{packets: 20});
+    const pcap = await Networking.Wifi.CaptureHandshake(mon,target.bssid);
+    if (pcap != null){
+        const password = await Crypto.Hashcat.Decrypt(`${pcap}`);
+        println(`SSID: ${target.ssid}`);
+        println(typeof(password));
+        println(`Password: ${password}`);
+        await FileSystem.WriteFile(`wifi.txt`,`placeholder`)
+        let content = await FileSystem.ReadFile(`wifi.txt`)
+        await FileSystem.WriteFile(`wifi.txt`,`${content} \n ${target.ssid}:${password}`)
+    }
    }
 };
-await Networking.Wifi.Deauth(mon,target.bssid,{packets: 20});
-const pcap = await Networking.Wifi.CaptureHandshake(mon,target.bssid);
-if (pcap != null){
-    const password = await Crypto.Hashcat.Decrypt(`/home/Midsubspace/${pcap}`);
-}
-println(`SSID: ${target.ssid}`);
-println(typeof(password))
-println(`Password: ${password}`);
